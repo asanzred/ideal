6595b7f5be02b4601a58f7efb4cbb517852636dc
Alberto Sanz Redondo <alberto.sanz@smallworldfs.com>
2016-12-09 15:51:06 +0100

    Comment routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::get('/ideal/issuers', 'IdealController@getissuers');
+/*Route::get('/ideal/issuers', 'IdealController@getissuers');
 Route::get('/ideal/send', 'IdealController@send');
-Route::get('/ideal/return', 'IdealController@returntx');
+Route::get('/ideal/return', 'IdealController@returntx');*/
